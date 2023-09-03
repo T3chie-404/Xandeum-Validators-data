8c23e02e321d2820caa8e9ac23143bf7b84fe1e3
WAGISDev <35645358+wagisdev@users.noreply.github.com>
2023-09-02 20:17:59 -0700

    Data capturing of validator basic information and config completed. Next steps is to query balance, voter information, etc.

--- Xandeum Validators/captureData.py
+++ Xandeum Validators/captureData.py
@@ -26,12 +26,15 @@
 # 888888888888888888888888888888888888888888888888888888888888888888888888888888
 
 import datetime
+from smtplib import quoteaddr
 import time
 import base64
 import requests
 import json
 import sys
 import os
+import mysql.connector
+from mysql.connector.constants import ClientFlag
 from dotenv import find_dotenv, load_dotenv
 
 # ------------------------------------------------------------------------------
@@ -41,15 +44,29 @@ from dotenv import find_dotenv, load_dotenv
 envPath = find_dotenv()
 load_dotenv(envPath)
 
+# Testing & Research settings
+setTest = os.getenv('setTest')
+
 # Validator Data Source Options
 dataSources = os.getenv('dataSources')
 dataSourcePorts = os.getenv('dataSourcePorts')
 
 # Data Store Configuration
 dbase = os.getenv('dbase')
+dbPort = os.getenv('dbPort')
 dbName = os.getenv('dbName')
 dbUname = os.getenv('dbUname')
 dbPass = os.getenv('dbPass')
+
+
+# Configure hard coded db connection here.
+db_conn = {
+    'host':dbase,
+    'database':dbName,
+    'port':dbPort,        
+    'user':dbUname,    
+    'password':dbPass
+    }
 
 #-------------------------------------------------------------------------------
 #
@@ -68,7 +85,8 @@ def main():
     starttime = startup()
     print ('Starup job @: {}'.format(starttime))
 
-    getClusterNodes()
+    payLoad = getClusterNodes()
+    
     
     stoptime = startup()    
     print ('Finished job @: {}'.format(stoptime))
@@ -117,10 +135,223 @@ def getClusterNodes():
 
     dataJSON = captureData(values)
     
+    print ('There are {} potential validators reporting in.'.format(len(dataJSON['result'])))
+    
+    payLoad = []        
     for item in dataJSON['result']:
-        print (item['pubkey'], item['gossip'], item['version'])
+        payLoadPrep = []
+        #print (item['pubkey'], item['gossip'], item['version'])
+        valID = item['pubkey']
+        valGoss = item['gossip']
+        valIP = valGoss.split(':', 1)[0]
+        valPort = valGoss.split(':', 1)[1]
+        valVer = item['version']
+        print ('Found Validatior ID {} \n\t'.format(valID), 
+               '-- IP Address: {} \n\t'.format(valIP),
+               '-- IP Port: {} \n\t'.format(valPort),
+               '-- RPC Address: {} \n\t'.format(valGoss),
+               '-- Validator Version: {} \n\n'.format(valVer))
+        payLoadPrep = (valID, valGoss, valIP, valPort, valVer)
+        payLoad.append(payLoadPrep)      
+
+    storeClusterNodes(payLoad)
+    
+    return(payLoad)
+
+def storeClusterNodes(payLoad):
+#-------------------------------------------------------------------------------
+# Name:        Function - storeClusterNodes
+# Purpose:  
+#-------------------------------------------------------------------------------
+
+    conn = mysql.connector.connect(**db_conn)    
+
+    for item in payLoad:
+        valID = item[0]
+        valGoss = item[1]
+        valIP = item[2]
+        valPort = item[3]
+        valVer = item[4]
+        
+        checkRes = checkIfValidatorExists(conn, valID)
+        #conn = mysql.connector.connect(**db_conn)        
+        if checkRes == 0:            
+            query = conn.cursor()
+            sqlCommand = '''
+
+            insert into validators (
+                validatorID
+                , firstCaptureDate
+                , sysCreateDate
+                , sysChangeDate
+                , globalID
+            )
+                Values ('{}', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), UUID())
+
+            '''.format(valID)
+
+            query.execute(sqlCommand)           
+            conn.commit()
+        else:   
+            conn = mysql.connector.connect(**db_conn)            
+            query = conn.cursor()
+            sqlCommand = '''
+
+            update validators
+            set sysChangeDate = CURRENT_TIMESTAMP()
+
+            '''.format(valID)
+
+            query.execute(sqlCommand)
+            conn.commit()
+            
+        itemFKID = gatherValidatorFKID(conn, valID)
+        captureValidatorConfig(conn, itemFKID, valGoss, valIP, valPort, valVer)
+        
+    conn.close()
+
+    return()
+
+def checkIfValidatorExists(conn, valID):
+#-------------------------------------------------------------------------------
+# Name:        Function - checkIfExists
+# Purpose:  
+#-------------------------------------------------------------------------------
+  
+    checkRes = 0
+    query = conn.cursor()
+    query_string = '''
+    
+    SELECT COUNT(validatorID) FROM validators 
+    WHERE validatorID = '{}'
+        
+    '''.format(valID)
+    
+    query.execute(query_string)
+    queryResult = query.fetchone()
+    
+    testItem = queryResult[0]
+    
+    if testItem == 0:
+        checkRes = 0
+    else: 
+        checkRes = 1
+        
+    query.close() 
+    
+    return(checkRes)
+
+
+def gatherValidatorFKID(conn, valID):
+#-------------------------------------------------------------------------------
+# Name:        Function - checkIfExists
+# Purpose:  
+#-------------------------------------------------------------------------------
+
+    query = conn.cursor()
+    query_string = '''
+    
+    SELECT globalID FROM validators 
+    WHERE validatorID = '{}'
+        
+    '''.format(valID)
+    
+    query.execute(query_string)
+    queryResult = query.fetchone()
+    
+    itemFKID = queryResult[0]
+        
+    query.close()
+
+    
+    return(itemFKID)
+
+def captureValidatorConfig(conn, itemFKID, valGoss, valIP, valPort, valVer):
+    
+    query = conn.cursor()
+    query_string = '''
+    
+    SELECT globalID FROM validatorsConfig 
+    WHERE fkID = '{}' order by sysChangeDate desc
+        
+    '''.format(itemFKID)
+ 
+    query.execute(query_string)
+    queryResult = query.fetchone() 
+    
+    if queryResult != None:
+        itemGLID = queryResult[0]
+        query = conn.cursor()
+        query_string = '''
+    
+        SELECT ipAddress FROM validatorsConfig 
+        WHERE globalID = '{}' order by sysChangeDate desc
+        
+        '''.format(itemGLID)
+ 
+        query.execute(query_string)
+        queryResult = query.fetchone()
+        itemIP = queryResult[0]
+        if itemIP != valIP:
+            query = conn.cursor()
+            sqlCommand = '''
+
+            insert into validatorsConfig (
+                fkID
+                , gossip
+                , ipAddress
+                , port
+                , validatorVersion
+                , sysCreateDate
+                , sysChangeDate
+                , globalID
+            )
+                Values ('{}', '{}', '{}', '{}', '{}', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), UUID())
+
+            '''.format(itemFKID, valGoss, valIP, valPort, valVer)
+
+            query.execute(sqlCommand)           
+            conn.commit()
+        else:
+            query = conn.cursor()
+            sqlCommand = '''
+
+            update validatorsConfig
+            set sysChangeDate = CURRENT_TIMESTAMP()
+                , port = '{}'
+                , gossip = '{}'
+                , validatorVersion = '{}'
+            where globalID = '{}'
+            '''.format(valPort, valGoss, valVer, itemGLID)
+            query.execute(sqlCommand)
+            conn.commit()                       
+    else:
+        query = conn.cursor()
+        sqlCommand = '''
+
+        insert into validatorsConfig (
+            fkID
+            , gossip
+            , ipAddress
+            , port
+            , validatorVersion
+            , sysCreateDate
+            , sysChangeDate
+            , globalID
+        )
+            Values ('{}', '{}', '{}', '{}', '{}', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), UUID())
+
+        '''.format(itemFKID, valGoss, valIP, valPort, valVer)
+
+        query.execute(sqlCommand)           
+        conn.commit()       
+        
+    query.close()
+    
     
     return()
+
+
 
 
 #-------------------------------------------------------------------------------
@@ -131,5 +362,11 @@ def getClusterNodes():
 #
 #-------------------------------------------------------------------------------
 
-if __name__ == "__main__":
-    main()
+if setTest == 0:
+    while True:
+        if __name__ == "__main__":
+            main()
+        time.sleep(180)
+else:
+    if __name__ == "__main__":
+        main()
